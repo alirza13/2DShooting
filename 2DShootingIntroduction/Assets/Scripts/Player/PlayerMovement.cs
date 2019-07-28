9aa8b84448ff17b4e6ed155afaf3ffbacfdf05ad
Alirza Guliyev <guliyev@sabanciuniv.edu>
2019-07-28 23:38:04 +0300

    Adding movement functionalities to the Player
    IsGrounded, jumping, joystick and movement was added.

---
+++ 2DShootingIntroduction/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Joystick joystick;
+    public Rigidbody2D rigBody;
+    public float jumpSpeed = 3f, moveSpeed = 2f;
+    public bool isGrounded;
+    public LayerMask groundLayers;
+    RaycastHit2D hit;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GroundDetection();
+        Move();
+    }
+
+    public void GroundDetection()
+    {
+        hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayers);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void Move()
+    {
+        if (joystick.Vertical > 0.2f && isGrounded)
+        {
+            rigBody.velocity = new Vector2(rigBody.velocity.x, jumpSpeed);
+        }
+        if (joystick.Horizontal > 0)
+        {
+            rigBody.velocity = new Vector2(moveSpeed, rigBody.velocity.y);
+        }
+        if (joystick.Horizontal < 0)
+        {
+            rigBody.velocity = new Vector2(-moveSpeed, rigBody.velocity.y);
+        }
+    }
+}
